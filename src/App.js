8f89292285e11ea2c3195e4edcae1a1e06aea5fc
quart-z <jtw2212@gmail.com>
2022-10-23 16:23:58 -0700

    Initial Commit, React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Home() {
+  return (
+    <body>
+      <header>
+
+      </header>
+      <div className = "main-content-box">
+      
+        {/* Section 1 - Content section , <Content /> */}
+
+      </div>
+    </body>
+  );
+}
+
+export default Home;
